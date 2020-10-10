7365bb9a0e21e29507b29ae16fe509bf45b7ecc6
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-10-09 22:50:48 -0300

    Initial Commit

---
+++ Chuck Norris Facts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chuck Norris Facts
+//
+//  Created by Djorkaeff Alexandre Vilela Pereira on 10/9/20.
+//  Copyright © 2020 Djorkaeff Alexandre Vilela Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
