759122406d28e2f8691278467498e93cff330d42
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-10-11 00:04:49 -0300

    chore: SwiftLint warnings

--- Chuck Norris Facts/App/SceneDelegate.swift
+++ Chuck Norris Facts/App/SceneDelegate.swift
@@ -15,7 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private var appCoordinator: AppCoordinator?
     private let disposeBag = DisposeBag()
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let scene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: scene)
