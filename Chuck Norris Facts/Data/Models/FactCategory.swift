5201276b540ecf34ff99e8e19deb87880f714c6f
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-10-20 19:00:39 -0300

    Facts Storage using Realm

---
+++ Chuck Norris Facts/Data/Models/FactCategory.swift
@@ -0,0 +1,21 @@
+//
+//  FactCategory.swift
+//  Chuck Norris Facts
+//
+//  Created by Djorkaeff Alexandre Vilela Pereira on 10/20/20.
+//  Copyright © 2020 Djorkaeff Alexandre Vilela Pereira. All rights reserved.
+//
+
+import Foundation
+
+struct FactCategory: Decodable {
+    let text: String
+    
+    init(text: String) {
+        self.text = text
+    }
+
+    init(from decoder: Decoder) throws {
+        self.text = try decoder.singleValueContainer().decode(String.self)
+    }
+}
